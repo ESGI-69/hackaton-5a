0eb5a31cb18fde8500ae1e82cef5ae7df9796bad
mrpinkcat <gatien.anizan@gmail.com>
2024-06-24 08:49:27 +0200

    fix: ignore console.log

--- backend/src/controllers/healthz.ts
+++ backend/src/controllers/healthz.ts
@@ -4,6 +4,7 @@ import { prisma } from '../db';
 export default async (req: Request, res: Response) => {
   try {
     await prisma.$connect();
+    // eslint-disable-next-line no-console
     console.log('Connected to the database');
     res.sendStatus(200);
   } catch (error) {
