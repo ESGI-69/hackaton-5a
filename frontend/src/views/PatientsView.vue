af82c3088eb7cf1987558c32aa90f0a62ca1b414
Izïa <izia.crinier@gmail.com>
2024-06-28 01:43:19 +0200

    add title on patients page

--- frontend/src/views/PatientsView.vue
+++ frontend/src/views/PatientsView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="mx-auto mt-8 px-4 sm:px-8">
+    <h1
+      class="text-2xl font-bold mb-8 text-contrast-500 border-b border-secondary-400"
+    >
+      Liste des patients
+    </h1>
     <SharedDatatable
       v-if="!patientStore.arePatientsLoading"
       :data="patientStore.patients"
